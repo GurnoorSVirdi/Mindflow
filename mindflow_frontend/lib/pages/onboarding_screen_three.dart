5fcc09443af96436ae0a4e4e733a2ecc242bbc1e
Aryan Jain <aryanjain422@gmail.com>
2023-11-28 22:49:53 -0500

    Finished first three onboarding screens.

---
+++ mindflow_frontend/lib/pages/onboarding_screen_three.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreenThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Hi Sam! Were you referred by a therapist?',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 32),
+              Text(
+                'If so, enter their 6-digit code below',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(6, (index) => _buildCodeBox()),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('need help?'),
+              ),
+              SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Continue'),
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("No, I wasn't referred by a therapist/provider"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCodeBox() {
+    return Container(
+      width: 40,
+      height: 50,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: TextFormField(
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(border: InputBorder.none),
+        // You may want to use a TextEditingController to handle the input
+      ),
+    );
+  }
+}
